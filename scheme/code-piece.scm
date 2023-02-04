d1026f7ab4e6a2dec86d9c7c7b6575256eeacfce
mugsimon <mug24375826x00a@outlook.jp>
2023-02-04 17:54:22 +0900

    first commit for mugsimon_lib.scm

--- scheme/code-piece.scm
+++ scheme/code-piece.scm
@@ -1,4 +1,4 @@
-;; スペース
+;; スペースで区切られた入力を逆順にしたリストを作成する
 (define (get-input-list)
   (let loop ((input (read))
              (input-list '()))
@@ -14,6 +14,41 @@
         sum
         (loop (cdr lst)
               (+ (car lst) sum)))))
-   
-(welcome-to-atcoder)
 
+;; make char list from s-char to e-char
+(define (make-char-lst s-char e-char)
+  (let ((s-char-int (char->integer s-char)))
+    (let loop ((char-lst '())
+               (e-char-int (char->integer e-char)))
+      (if (= e-char-int s-char-int)
+          (cons (integer->char e-char-int) char-lst)
+          (loop (cons (integer->char e-char-int)
+                      char-lst)
+                (- e-char-int 1))))))
+
+;; make A to Z char list
+(define (make-AtoZ-char-lst)
+  (make-char-lst #\A #\Z))
+
+(define (make-vector2 k1 k2)
+  (make-vector k1 (make-vector k2)))
+
+(define (vector2-ref vec2 k1 k2)
+  (vector-ref (vector-ref vec2 k1) k2))
+
+(define (make-vectors . k-lst)
+  (let ((k-lst (reverse k-lst)))
+    (let loop ((k-lst (cdr k-lst))
+               (vectors (make-vector (car k-lst))))
+      (if (null? k-lst)
+          vectors
+          (loop (cdr k-lst)
+                (make-vector (car k-lst) vectors))))))
+
+(define (vectors-ref vecs . k)
+  (let loop ((vecs vecs)
+             (k k))
+    (if (null? k)
+        vecs
+        (loop (vector-ref (car k))
+              (cdr k)))))
--- scheme/mugsimon_lib.scm
+++ scheme/mugsimon_lib.scm
@@ -2,3 +2,57 @@
 ;; convert char to symbol
 (define (char->symbol ch)
   (string->symbol (string ch)))
+
+(define (string-split-values str dlr)
+  (apply values (string-split str dlr)))
+
+;; 
+#;(define (string-split str . dlr)
+  (if (string? str)
+      (let ((dlr (if (null? dlr) " " (car dlr)))
+            (str-len (string-length str)))
+        (let loop ((start (- str-len 1))
+                   (end str-len)
+                   (str-lst '()))
+          (cond ((zero? start)
+                 (cons (substring str start end) str-lst))
+                ((and (string=? (substring str start (+ start 1)) dlr)
+                      (= (+ start 1) end))
+                 (loop (- start 1)
+                       (- end 1)
+                       str-lst))
+                ((string=? (substring str start (+ start 1)) dlr)
+                 (loop (- start 1)
+                       start
+                       (cons (substring str (+ start 1) end) str-lst)))
+                (else
+                 (loop (- start 1)
+                       end
+                       str-lst)))))
+      (error "string required, but got" str)))
+
+(define (expt b n)
+  (cond ((= n 0)
+         1)
+        ((even? n)
+         (square (expt b (/ n 2))))
+        (else
+         (* b (expt b (- n 1))))))
+
+(define (get-digit n . dgt)
+  (if (null? (cdr dgt))
+      (let* ((dgt (car dgt))
+             (div-num (expt 10 (- dgt 1)))
+             (mod-num (expt 10 dgt)))
+        (div (mod n mod-num) div-num))
+      (let* ((dgt-min (car dgt))
+             (dgt-max (cadr dgt))
+             (mod-num-max (expt 10 dgt-max))
+             (mod-num-min (expt 10 (- dgt-min 1))))
+        (div (- (mod n mod-num-max)
+                (mod n mod-num-min))
+             mod-num-min))))
+
+
+
+        
